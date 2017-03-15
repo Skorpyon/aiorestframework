0011fcbe6e9ea55b7594dc72fcb56b4725c269e0
anton <anton.trishenkov@gmail.com>
2017-03-15 04:38:12 +0300

    Add data attribute to Response

--- aiorestframework/response.py
+++ aiorestframework/response.py
@@ -1,3 +1,4 @@
+import ujson as json
 from aiohttp.web import Response as AiohttpResponse
 
 
@@ -6,10 +7,11 @@ class Response(AiohttpResponse):
     Response class passed application/json content_type as default
     """
 
-    def __init__(self, *, body=None, status=200,
+    def __init__(self, *, data=None, status=200, body=None,
                  reason=None, text=None, headers=None, content_type=None,
                  charset=None):
         content_type = 'application/json' if content_type is None else content_type
+        text = json.dumps(data) if (data and not body and not text) else text
         super().__init__(body=body, status=status, reason=reason, text=text,
                          headers=headers, content_type=content_type,
                          charset=charset)
