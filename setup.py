1f4b061f3acf0062b25f707876ca8ba16e7afab4
anton <anton.trishenkov@gmail.com>
2017-04-20 07:59:36 +0300

    update requirements

--- setup.py
+++ setup.py
@@ -40,9 +40,6 @@ setup(
     zip_safe=True,
     install_requires=[
         'ujson==1.35',
-        'aiohttp==1.3.3',
-        'trafaret>=0.7.5',
-        'trafaret_config>=1.0.1',
-        'watchdog>=0.8.3',
+        'aiohttp>=2.0.7',
     ],
 )
