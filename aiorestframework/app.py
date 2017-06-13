8cd2b0bacc57af87cb8ba448a4ab71c074e2b40b
anton <anton.trishenkov@gmail.com>
2017-06-13 08:33:52 +0300

    Update client_max_size settings

--- aiorestframework/app.py
+++ aiorestframework/app.py
@@ -10,12 +10,15 @@ __all__ = (
 
 
 class APIApplication(Application):
-    def __init__(self, *, name='', logger=web_logger, loop=None,
-                 router=None, middlewares=(), debug=...):
+    def __init__(self, *, name='', logger=web_logger, router=None,
+                 middlewares=(), handler_args=None, client_max_size=1024**2,
+                 loop=None, debug=...):
         self.name = name
         if router is None:
             router = APIUrlDispatcher()
         assert isinstance(router, APIUrlDispatcher), router
         setattr(router, 'app_name', name)
-        super().__init__(logger=logger, loop=loop, router=router,
-                         middlewares=middlewares, debug=debug)
+        super().__init__(
+            logger=logger, router=router, middlewares=middlewares,
+            handler_args=handler_args, client_max_size=client_max_size,
+            loop=loop, debug=debug)
